609881f0e6e7fd1ea8196c8aeb605f2a54d9d221
karmjeetmor73@gmail.com <karmjeetmor73@gmail.com>
2024-09-09 18:00:36 +0530

    first commit

---
+++ src/JavaProjects/Collections.java
@@ -0,0 +1,33 @@
+package JavaProjects;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+public class Collections {
+//	List<String> arr=new ArrayList();
+	
+	
+
+	public static void main(String[] args) {
+		ArrayList<String> arr=new ArrayList<>();
+		arr.add("karmjeet");
+		arr.add("mor");
+
+	
+		arr.add("jeet");
+		arr.add("mor1");
+		Collections.sort(arr);
+//		System.out.println(arr);
+		for(Object i:arr) {
+			System.out.println(i);
+			
+
+	
+	
+	
+		}// TODO Auto-generated method stub
+
+	}
+
+}
